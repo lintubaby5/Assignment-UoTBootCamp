7c5b802ad570149700b4935623500417cfd02499
lintubaby5 <119901094+lintubaby5@users.noreply.github.com>
2023-01-17 16:23:02 -0500

    Adding final version of PyPoll Assignment

--- python-challenge/PyPoll/main.py
+++ python-challenge/PyPoll/main.py
@@ -1,13 +1,17 @@
 import os
 import csv
+from collections import Counter
 
 #file path for csv input file
 csv_path=os.path.join(R"Resources","election_data.csv")
 #declare variables to zero
 countvotes=0
+total_candidates=0
 Candidates=[]
-votes=[]
-
+DistinctCandidates=[]
+Candidatevotes=[]
+HighestVotes=0
+HighestVoteCandidate=""
 #read from csv file
 with open(csv_path,encoding='utf-8-sig') as csvfile:
     csvreader=csv.reader(csvfile,delimiter=',')
@@ -16,7 +20,47 @@ with open(csv_path,encoding='utf-8-sig') as csvfile:
     for row in csvreader:
         #count each row to find total votes
         countvotes=countvotes+1
-        if(row[2] not in Candidates):
-            Candidates.append(row[2])
-         
+        #add the candidate to list
+        Candidates.append(row[2])
+    #find the counter for each Candidates and store it in a dict
+    result=Counter(Candidates)
+    #convert dictionary to lists
+    for key, value in result.items():
+        Candidate=key
+        Votes=value
+        DistinctCandidates.append(Candidate)
+        Candidatevotes.append(Votes)
+    #find total condidates
+    total_candidates=len(DistinctCandidates)
+    #print headers
+    print("Election results")
+    print("-------------------------")
+    print(f"Total Votes: {countvotes}")
+    print("-------------------------")
+    #iterate through lists to print the Candidates and the Votes
+    for i in range(total_candidates):
+        CandidateiPercent=round(int(Candidatevotes[i])/int(countvotes)*100,3)
+        print(f"{DistinctCandidates[i]}: {CandidateiPercent}% ({Candidatevotes[i]})")
+        if(Candidatevotes[i]>HighestVotes):
+            HighestVotes=Candidatevotes[i]
+            HighestVoteCandidate=DistinctCandidates[i]
+    print("-------------------------")
+    print(f"Winner: {HighestVoteCandidate}")
+    print("-------------------------")
+#declare the text file to write the output to
+txtfilepath=os.path.join(r"Analysis","outputPyPoll.txt")
+with open(txtfilepath,'w') as txtfile:
+    #write into the file
+    txtfile.write("Election results\n")
+    txtfile.write("-------------------------\n")
+    txtfile.write(f"Total Votes: {countvotes}\n")
+    txtfile.write("-------------------------\n")
+    for i in range(total_candidates):
+        CandidateiPercent=round(int(Candidatevotes[i])/int(countvotes)*100,3)
+        txtfile.write(f"{DistinctCandidates[i]}: {CandidateiPercent}% ({Candidatevotes[i]})\n")
+    txtfile.write("-------------------------\n")
+    txtfile.write(f"Winner: {HighestVoteCandidate}\n")
+    txtfile.write("-------------------------\n")
+    
+
 
---
+++ python-challenge/README.md
@@ -0,0 +1,70 @@
+## PyBank and PyPoll
+
+PyBank: Analysis on monthly Profit/Loss data
+PyPoll: Analysis on election result
+
+
+## PyBank
+
+In this challenge, I have created a Python Script to analyze the financial records of your company. The resource file has the budget data month wise, number of Profit/Losses
+
+Created a script that analyzes the records to calculate each of the following:
+
+* The total number of months included in the dataset
+
+* The net total amount of "Profit/Losses" over the entire period
+
+* The changes in "Profit/Losses" over the entire period, and then the average of those changes
+
+* The greatest increase in profits (date and amount) over the entire period
+
+* The greatest decrease in profits (date and amount) over the entire period
+
+Analysis Output :
+
+  ```text
+  Financial Analysis
+  ----------------------------
+  Total Months: 86
+  Total: $22564198
+  Average Change: $-8311.11
+  Greatest Increase in Profits: Aug-16 ($1862002)
+  Greatest Decrease in Profits: Feb-14 ($-1825558)
+  ```
+
+In addition, the final script prints the analysis to the terminal and export a text file with the results.
+
+## PyPoll
+
+In this challenge, I have created a Python Script to analyze the election results of a Poll. The resource file has the poll data that contains Columns Voter ID, County and Candidate
+
+The task was to create a Python script that analyzes the votes and calculates each of the following:
+
+* The total number of votes cast
+
+* A complete list of candidates who received votes
+
+* The percentage of votes each candidate won
+
+* The total number of votes each candidate won
+
+* The winner of the election based on popular vote.
+
+Analysis Output:
+
+
+  ```text
+  Election Results
+  -------------------------
+  Total Votes: 369711
+  -------------------------
+  Charles Casper Stockham: 23.049% (85213)
+  Diana DeGette: 73.812% (272892)
+  Raymon Anthony Doane: 3.139% (11606)
+  -------------------------
+  Winner: Diana DeGette
+  -------------------------
+  ```
+
+In addition, the final script prints the analysis to the terminal and export a text file with the results.
+
